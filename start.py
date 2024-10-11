eea73eb3dbcb812e0225d47c0abb194c5ba94dc6
Niilo Torppa <nieetorp@jalava.cc.jyu.fi>
2024-10-11 16:39:17 +0300

    asetaAihe palauttaa nyt myös satunnaisen kysymyslistan aiheesta. haeKysymys palauttaa kysymyksen ID:n perusteella. Raulin vastaus-polku lisätty.

--- start.py
+++ start.py
@@ -2,7 +2,7 @@
 from flask import Flask, request, redirect, render_template, session, url_for
 from flask_cors import CORS
 from functools import wraps
-import time, json, math
+import time, json, math, random
 
 app = Flask(__name__)
 CORS(app)
@@ -47,7 +47,7 @@ def annaID():
 # return onnistuiko?
 @app.route('/asetaAihe', methods=['POST'])
 def asetaAihe():
-    id = request.json['userID']
+    id = request.json['kayttajaID']
     aihe = request.json['aihe']
 
     with open('users.json', 'r') as users:
@@ -58,53 +58,46 @@ def asetaAihe():
     with open('users.json', 'w') as users:
         json.dump(data, users, indent=2)
 
-    return data[id], 200
+    kysymykset = []
+    with open('kysymykset.json',  'r') as qF:
+        qData = json.load(qF)
+    
+    for q in qData[aihe]:
+        kysymykset.append(q)
+
+    random.shuffle(kysymykset)
+
+    return { "aiheData": data[id], "kysymykset": kysymykset }, 200
 
 
 @app.route('/haeKysymys', methods=['POST'])
 def haeKysymys():
-    print("moi")
+    userID = request.json['kayttajaID']
+    qID = request.json['kysymysID']
+    aihe = request.json['aihe']
+
+    with open('kysymykset.json', 'r') as kysym:
+        data = json.load(kysym)
+        kysymys = data[aihe][qID]
+        del kysymys['o']
+
+    return kysymys, 200
 
 
 
+# Tarkistaa annetun vastauksen
+# attr: { vastaus, kysymysID, kayttajaID}
+# return oikeiden määrä?
+@app.route('/vastaus', methods=['POST'])
+def tarkistaVastaus():
+    vastaus = request.json['vastaus']
+    kysymysID = request.json['kysymysID']
+    kayttajaID = request.json['kayttajaID'] #tai aihe?
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#alhaalla :]
+    with open('kysymykset.json', 'r') as kysymykset:
+        data = json.load(kysymykset)
+    
+    # hae tässä datasta haluttu tieto, eli oikeiden vastausten taulukko
+    # tarkista
+    # palauta mitä?
+    return data
--- testing/backendTest.js
+++ testing/backendTest.js
@@ -3,15 +3,35 @@ window.onload = async () => {
     const uId = await (await fetch("../main.cgi/annaID")).text();
 
     const obj = {
-        userID: uId,
-        aihe: "aa"
+        kayttajaID: uId,
+        aihe: "history"
     }
 
-    console.log(await (await fetch("../main.cgi/asetaAihe", {
+    resp = await (await fetch("../main.cgi/asetaAihe", {
         method: "POST",
         body: JSON.stringify(obj),
         headers: {
             "Content-Type": "application/json"
         }
-    })).json());
+    })).json();
+
+    console.log(resp);
+    let aihe = resp["aiheData"]["aihe"];
+    let kysymykset = resp["kysymykset"];
+
+
+
+    const kysymHaku = {
+        kayttajaID: uId,
+        aihe: aihe,
+        kysymysID: kysymykset[0]
+    }
+
+    await (await fetch("../main.cgi/haeKysymys", {
+        method: "POST",
+        body: JSON.stringify(kysymHaku),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }));
 }
