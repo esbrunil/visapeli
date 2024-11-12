c6800d31844ef2c0c2bf71d635e079fb1f2377dc
Niilo Torppa <nieetorp@jalava.cc.jyu.fi>
2024-11-13 00:49:23 +0200

    päivitys

--- testing/backendTest.js
+++ testing/backendTest.js
@@ -4,13 +4,16 @@ let uId;
 
 window.onload = async () => {
 
+    // Requesti "annaID"
     const uId = await (await fetch("../main.cgi/annaID")).text();
 
+    // Tehdään objekti, jossa käyttäjän id ja aiheena hissa
     const obj = {
         kayttajaID: uId,
         aihe: "history"
     }
 
+    // Requesti - asetetaan aiheeksi edellisen objektin aihe
     resp = await (await fetch("../main.cgi/asetaAihe", {
         method: "POST",
         body: JSON.stringify(obj),
@@ -19,18 +22,20 @@ window.onload = async () => {
         }
     })).json();
 
+    // Printataan vastaus, asetetaan aiheeksi vastauksesta saatu aihe ja kysymyksiin lista kysymysten id:istä
     console.log(resp);
     let aihe = resp["aiheData"]["aihe"];
     let kysymykset = resp["kysymykset"];
 
 
-
+    // Olio kysymysten hakuun
     const kysymHaku = {
         kayttajaID: uId,
         aihe: aihe,
         kysymysID: kysymykset[0]
     }
 
+    // Requesti kysymyksen hakemiselle annetulla id:llä
     await (await fetch("../main.cgi/haeKysymys", {
         method: "POST",
         body: JSON.stringify(kysymHaku),
@@ -39,13 +44,14 @@ window.onload = async () => {
         }
     }));
 
+    // Lähetetään heartbeat joka 3. sekunti
     setInterval(async () => {
         console.log("interval");
         await heartbeat();
     }, 3000);
 
+    // Heartbeat-toteutus
     const heartbeat = async () => {
-
         const object = {
             kayttajaID: uId
         }
